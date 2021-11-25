82eb3d4b5025da98248bb3825976bd439f93c0bf
Orlando Lopes <78795585+Kashigu@users.noreply.github.com>
2021-11-25 19:57:38 +0000

    Login on functions

--- includes/body.inc.php
+++ includes/body.inc.php
@@ -55,6 +55,10 @@ function top($menu = HOME){
             <div class="col-lg-4">
                 <div class="header__nav">
                     <nav class="header__menu mobile-menu">
+                        <?php
+                        session_start();
+                        if(!isset($_SESSION['id'])){
+                        ?>
                         <ul>
                             <li <?php if ($menu == HOME) echo "class=\"active\""; ?>><a href="index.php">Homepage</a>
                             </li>
@@ -62,19 +66,175 @@ function top($menu = HOME){
                                         href="procurar.php">Animes </a>
                             </li>
                         </ul>
+                        <?php
+                        }else{
+                            $con = mysqli_connect(HOST, USER, PASSWORD, DATABASE);
+                            $sql = "select * from users where userId=" . $_SESSION['id'];
+                            $resultPerfis = mysqli_query($con, $sql);
+                            $dadosPerfis = mysqli_fetch_array($resultPerfis);
+
+                        if ($_SESSION['id'] and $dadosPerfis['usersAdmin'] == 'Admin') {
+
+                        ?>
+                        <ul>
+                            <li <?php if ($menu == HOME) echo "class=\"active\""; ?>><a href="index.php">Homepage</a>
+                            </li>
+                            <li <?php if ($menu == PROCURAR) echo "class=\"active\""; ?>><a
+                                        href="procurar.php">Animes </a>
+                            <li><a href="admin/index.php">Site Management</a>
+                            </li>
+                        </ul>
+                        <?php
+                        }else if (($_SESSION['id']) and $dadosPerfis['usersAdmin'] =='User'){
+                        ?>
+                        <ul>
+                            <li <?php if ($menu == HOME) echo "class=\"active\""; ?>><a href="index.php">Homepage</a>
+                            </li>
+                            <li <?php if ($menu == PROCURAR) echo "class=\"active\""; ?>><a
+                                        href="procurar.php">Animes </a>
+                            </li>
+                        </ul>
+                        <?php
+                        }
+                        }
+                        ?>
+
                     </nav>
                 </div>
             </div>
-            <div class="col-lg-4">
+            <div class="col-lg-3">
                 <div class="header__right">
-                    <form action="search.php" style="position: relative; right: 70px ; top: 30px" method="post"
+                    <form action="../search.php" style="position: relative; right: 70px ; top: 30px" method="post"
                           enctype="multipart/form-data" id="searchProcurar">
-                        <input style="margin-right:10px; color: white; background-color: #0b0c2a; display: none "
+                        <input style="margin-right:-50px; color: white; background-color: #0b0c2a; display: none "
                                id="Search" type="text" placeholder="Search.." name="search">
                     </form>
-                    <button style="background-color:transparent; border:transparent" onclick="mostrarSearch()"><span
+                    <button style="background-color:transparent; border:transparent; margin-right: -90px; width: 50px; position: absolute; right: 70px ; top: 50px" onclick="mostrarSearch()"><span
                                 class="icon_search" style="color: white; margin-right: 10px"></span></button>
-                    <a href="login.php"><span class="icon_profile"></span></a>
+                </div>
+            </div>
+            <div class="col-lg-1">
+                <div class="header__right">
+                    <?php
+                    if (!isset($_SESSION['id'])) {
+                        ?>
+                        <a href="../login.php"><span class="icon_profile"></span></a>
+                        <?php
+                    } else {
+                        ?>
+                        <?php
+                        $con = mysqli_connect(HOST, USER, PASSWORD, DATABASE);
+                        $sql = "select * from users where userId=" . $_SESSION['id'];
+                        $resultPerfis = mysqli_query($con, $sql);
+                        $dadosPerfis = mysqli_fetch_array($resultPerfis)
+                        ?>
+                        <a href="../perfil.php?id=<?php echo $dadosPerfis['userId'] ?>">
+                            <img src="<?php echo $dadosPerfis['userImageURL'] ?>" class="reduzido"></a>
+                        <?php
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+        <div id="mobile-menu-wrap"></div>
+    </div>
+</header>
+<?php
+}
+function topadmin($menu = HOME){
+?>
+
+    <!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Anime Template">
+    <meta name="keywords" content="Anime, unica, creative, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Anime | Template</title>
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet">
+
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="../css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="../css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="../css/elegant-icons.css" type="text/css">
+    <link rel="stylesheet" href="../css/plyr.css" type="text/css">
+    <link rel="stylesheet" href="../css/nice-select.css" type="text/css">
+    <link rel="stylesheet" href="../css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="../css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="../css/style.css" type="text/css">
+</head>
+
+<body>
+<!-- Page Preloder -->
+<div id="preloder">
+    <div class="loader"></div>
+</div>
+
+<!-- Header Section Begin -->
+<header class="header">
+    <div class="container">
+        <div class="row col-lg-12">
+            <div class="col-lg-4">
+                <div class="header__logo">
+                    <a href="../index.php">
+                        <img src="../img/logo.png" alt="">
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="header__nav">
+                    <nav class="header__menu mobile-menu">
+                        <ul>
+                            <li><a href="../index.php">Homepage</a>
+                            </li>
+                            <li><a href="../procurar.php">Animes </a>
+                            </li>
+                            <li <?php if ($menu == GESTAO) echo "class=\"active\""; ?>><a href="index.php">Site
+                                    Management </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="header__right">
+                    <form action="../search.php" style="position: relative; right: 70px ; top: 30px" method="post"
+                          enctype="multipart/form-data" id="searchProcurar">
+                        <input style="margin-right:-50px; color: white; background-color: #0b0c2a; display: none "
+                               id="Search" type="text" placeholder="Search.." name="search">
+                    </form>
+                    <button style="background-color:transparent; border:transparent; margin-right: -90px; width: 50px; position: absolute; right: 70px ; top: 50px" onclick="mostrarSearch()"><span
+                                class="icon_search" style="color: white; margin-right: 10px"></span></button>
+                </div>
+            </div>
+            <div class="col-lg-1">
+                <div class="header__right">
+                    <?php
+                    session_start();
+                    if (!isset($_SESSION['id'])) {
+                        ?>
+                        <a href="../login.php"><span class="icon_profile"></span></a>
+                        <?php
+                    } else {
+                        ?>
+                        <?php
+                        $con = mysqli_connect(HOST, USER, PASSWORD, DATABASE);
+                        $sql = "select * from users where userId=" . $_SESSION['id'];
+                        $resultPerfis = mysqli_query($con, $sql);
+                        $dadosPerfis = mysqli_fetch_array($resultPerfis)
+                        ?>
+                        <a href="../perfil.php?id=<?php echo $dadosPerfis['userId'] ?>">
+                            <img src="../<?php echo $dadosPerfis['userImageURL'] ?>" class="reduzido"></a>
+                        <?php
+                    }
+                    ?>
                 </div>
             </div>
         </div>
@@ -146,87 +306,8 @@ function bot(){
 </html>
 <?php
 }
-function topadmin($menu = HOME){
-?>
 
-    <!DOCTYPE html>
-<html lang="zxx">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="description" content="Anime Template">
-    <meta name="keywords" content="Anime, unica, creative, html">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Anime | Template</title>
-
-    <!-- Google Font -->
-    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;400;500;600;700;800;900&display=swap"
-          rel="stylesheet">
-
-    <!-- Css Styles -->
-    <link rel="stylesheet" href="../css/bootstrap.min.css" type="text/css">
-    <link rel="stylesheet" href="../css/font-awesome.min.css" type="text/css">
-    <link rel="stylesheet" href="../css/elegant-icons.css" type="text/css">
-    <link rel="stylesheet" href="../css/plyr.css" type="text/css">
-    <link rel="stylesheet" href="../css/nice-select.css" type="text/css">
-    <link rel="stylesheet" href="../css/owl.carousel.min.css" type="text/css">
-    <link rel="stylesheet" href="../css/slicknav.min.css" type="text/css">
-    <link rel="stylesheet" href="../css/style.css" type="text/css">
-</head>
-
-<body>
-<!-- Page Preloder -->
-<div id="preloder">
-    <div class="loader"></div>
-</div>
-
-<!-- Header Section Begin -->
-<header class="header">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-4">
-                <div class="header__logo">
-                    <a href="../index.php">
-                        <img src="../img/logo.png" alt="">
-                    </a>
-                </div>
-            </div>
-            <div class="col-lg-4">
-                <div class="header__nav">
-                    <nav class="header__menu mobile-menu">
-                        <ul>
-                            <li><a href="../index.php">Homepage</a>
-                            </li>
-                            <li><a href="../procurar.php">Animes </a>
-                            </li>
-                            <li <?php if ($menu == GESTAO) echo "class=\"active\""; ?>><a href="index.php">Site
-                                    Management </a>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-            <div class="col-lg-4">
-                <div class="header__right">
-                    <form action="../search.php" style="position: relative; right: 70px ; top: 30px" method="post"
-                          enctype="multipart/form-data" id="searchProcurar">
-                        <input style="margin-right:10px; color: white; background-color: #0b0c2a; display: none "
-                               id="Search" type="text" placeholder="Search.." name="search">
-                    </form>
-                    <button style="background-color:transparent; border:transparent" onclick="mostrarSearch()"><span
-                                class="icon_search" style="color: white; margin-right: 10px"></span></button>
-                    <a href="../login.php"><span class="icon_profile"></span></a>
-                </div>
-            </div>
-        </div>
-        <div id="mobile-menu-wrap"></div>
-    </div>
-</header>
-<?php
-}
-function botadmin($menu=HOME){
+function botadmin($menu = HOME){
 ?>
 <footer class="footer">
     <div class="page-up">
