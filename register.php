bdaf111e98fec27383c7d778f5a137c06ca7d997
Orlando Lopes <78795585+Kashigu@users.noreply.github.com>
2021-12-08 22:37:41 +0000

    begin of register

--- register.php
+++ register.php
@@ -8,13 +8,14 @@ top(ANIMES);
             <div class="row">
                 <div class="col-lg-12 text-center">
                     <div class="normal__breadcrumb__text">
-                        <h2>Login</h2>
+                        <h2>Register</h2>
                         <p>Welcome to the official Anime.</p>
                     </div>
                 </div>
             </div>
         </div>
     </section>
+
     <!-- Normal Breadcrumb End -->
 
     <!-- Login Section Begin -->
@@ -23,14 +24,17 @@ top(ANIMES);
             <div class="row">
                 <div class="col-lg-6">
                     <div class="login__form">
-                        <h3>Login</h3>
+                        <h3>Register</h3>
                         <form action="confirmaLogin.php" method="post" class="contact-form" id="frmConfirma">
+                            <div class="input__item mt-3">
+                                <input style="color: black" type="text" id="name" name="name" placeholder="UserName">
+                                <i class="bi bi-person-fill"></i>
+                            </div>
                             <div class="input__item mt-3">
                                 <input style="color: black" type="email" id="emails" name="email" placeholder="Email address">
                                 <span class="icon_mail"></span>
                             </div>
                             <div class="input__item mt-3">
-
                                 <input style="color: black;" type="password" id="password" name="password" placeholder="Password">
                                 <span class="icon_lock"></span>
                             </div>
@@ -41,8 +45,8 @@ top(ANIMES);
                 </div>
                 <div class="col-lg-6">
                     <div class="login__register">
-                        <h3>Dont’t Have An Account?</h3>
-                        <a href="register.php" class="primary-btn">Register Now</a>
+                        <h3>Already have an Account ?</h3>
+                        <a href="login.php" class="primary-btn">Sign In</a>
                     </div>
                 </div>
             </div>
