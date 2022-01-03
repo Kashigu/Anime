cc770531599ef780540b0687a30d40051b638606
Orlando Lopes <78795585+Kashigu@users.noreply.github.com>
2022-01-03 13:20:26 +0000

    D:

--- procurar.php
+++ procurar.php
@@ -33,26 +33,26 @@ top(PROCURAR)
                             $sql = "select * from Anime";
                             $resultAnime = mysqli_query($con, $sql);
                             while ($dadosAnime = mysqli_fetch_array($resultAnime)) {
-                                ?>
-                                <div class="col-lg-4 col-md-6 col-sm-6">
-                                    <a href="anime-details.php?id=<?php echo $dadosAnime['animeId'] ?>">
+                            ?>
+                            <div class="col-lg-4 col-md-6 col-sm-6">
+                                <a href="anime-details.php?id=<?php echo $dadosAnime['animeId'] ?>">
                                     <div class="product__item">
                                         <div class="product__item__pic set-bg"
                                              data-setbg="<?php echo $dadosAnime['animeImagemURL'] ?>">
                                             <?php
-                                                $sql1 = "Select count(*) as n from episodios where episodioAnimeId=" . $dadosAnime['animeId'];
-                                                $resultadoEpisodios = mysqli_query($con, $sql1);
-                                                $dadosEp=mysqli_fetch_array($resultadoEpisodios);
-                                                ?>
-                                            <div class="ep"><?php echo $dadosEp['n']?> /
+                                            $sql1 = "Select count(*) as n from episodios where episodioAnimeId=" . $dadosAnime['animeId'];
+                                            $resultadoEpisodios = mysqli_query($con, $sql1);
+                                            $dadosEp = mysqli_fetch_array($resultadoEpisodios);
+                                            ?>
+                                            <div class="ep"><?php echo $dadosEp['n'] ?> /
                                                 <?php
                                                 $sql = "select * from episodios where episodioAnimeId=" . $dadosAnime['animeId'];
                                                 $resultep = mysqli_query($con, $sql);
-                                                $dadosip =mysqli_fetch_array($resultep);
-                                                if(mysqli_affected_rows($con)=='')
+                                                $dadosip = mysqli_fetch_array($resultep);
+                                                if (mysqli_affected_rows($con) == '')
                                                     echo '0';
                                                 else
-                                                    echo $dadosip['episodioTotal']?> </div>
+                                                    echo $dadosip['episodioTotal'] ?> </div>
                                             <div class="comment"><i class="fa fa-comments"></i> 11</div>
                                             <div class="view"><i class="fa fa-eye"></i> 9141</div>
                                         </div>
@@ -60,7 +60,7 @@ top(PROCURAR)
                                             <ul>
                                                 <?php
                                                 $sql = "select * from animeCategorias inner join Anime on categoriaAnimeId = animeId
-                                                                inner join Categorias on animeCategoriaId = categoriaId where animeId=".$dadosAnime['animeId'];
+                                                                inner join Categorias on animeCategoriaId = categoriaId where animeId=" . $dadosAnime['animeId'];
                                                 $resultCategorias = mysqli_query($con, $sql);
                                                 while ($dadosCategorias = mysqli_fetch_array($resultCategorias)) {
                                                     ?>
@@ -70,128 +70,122 @@ top(PROCURAR)
                                                 ?>
                                             </ul>
                                             <h5><?php echo $dadosAnime['animeName'] ?></a></h5>
-                                        </div>
-                                    </div>
-                                </div>
-                                <?php
-                            }
-                            ?>
+                            </div>
                         </div>
                     </div>
-                    <div class="product__pagination">
-                        <a href="#" class="current-page">1</a>
-                        <a href="#">2</a>
-                        <a href="#">3</a>
-                        <a href="#">4</a>
-                        <a href="#">5</a>
-                        <a href="#"><i class="fa fa-angle-double-right"></i></a>
+                    <?php
+                    }
+                    ?>
+                </div>
+            </div>
+            <div class="product__pagination">
+                <a href="#" class="current-page">1</a>
+                <a href="#">2</a>
+                <a href="#">3</a>
+                <a href="#">4</a>
+                <a href="#">5</a>
+                <a href="#"><i class="fa fa-angle-double-right"></i></a>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-8">
+            <div class="product__sidebar">
+                <div class="product__sidebar__view">
+                    <div class="section-title">
+                        <h5>Top Views</h5>
+                    </div>
+                    <div class="filter__gallery">
+                        <?php
+                        $sql = "select * from anime";
+                        $resultANIMES = mysqli_query($con, $sql);
+                        $maximo = 0;
+                        while ($dadosANIMES = mysqli_fetch_array($resultANIMES)) {
+                            ?>
+                            <div class="product__sidebar__view__item set-bg mix day years"
+                                 data-setbg="<?php echo $dadosANIMES['animeImagemURL'] ?>">
+                                <?php
+                                $sql1 = "Select count(*) as n from episodios where episodioAnimeId=" . $dadosANIMES['animeId'];
+                                $resultadoEpisodios = mysqli_query($con, $sql1);
+                                $dadosEp = mysqli_fetch_array($resultadoEpisodios);
+                                ?>
+                                <div class="ep"><?php echo $dadosEp['n'] ?> / <?php
+                                    $sql = "select * from episodios where episodioAnimeId=" . $dadosANIMES['animeId'];
+                                    $resultep = mysqli_query($con, $sql);
+                                    $dadosip = mysqli_fetch_array($resultep);
+                                    if (mysqli_affected_rows($con) == '')
+                                        echo '0';
+                                    else
+                                        echo $dadosip['episodioTotal'] ?></div>
+                                <div class="view"><i class="fa fa-eye"></i> <?php echo 99999//$dadosANIMES['redeTipe'] ?></div>
+                                <h5>
+                                    <a href="anime-details.php?id=<?php echo $dadosANIMES['animeId'] ?>"><?php echo $dadosANIMES['animeName'] ?></a>
+                                </h5>
+                            </div>
+                            <?php
+                            $maximo++;
+                        }
+                        ?>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6 col-sm-8">
-                    <div class="product__sidebar">
-                        <div class="product__sidebar__view">
-                            <div class="section-title">
-                                <h5>Top Views</h5>
-                            </div>
-                            <ul class="filter__controls">
-                                <li class="active" data-filter="*">Day</li>
-                                <li data-filter=".week">Week</li>
-                                <li data-filter=".month">Month</li>
-                                <li data-filter=".years">Years</li>
-                            </ul>
-                            <div class="filter__gallery">
-                                <div class="product__sidebar__view__item set-bg mix day years"
-                                     data-setbg="img/sidebar/tv-1.jpg">
-                                    <div class="ep">18 / ?</div>
-                                    <div class="view"><i class="fa fa-eye"></i> 9141</div>
-                                    <h5><a href="#">Boruto: Naruto next generations</a></h5>
-                                </div>
-                                <div class="product__sidebar__view__item set-bg mix month week"
-                                     data-setbg="img/sidebar/tv-2.jpg">
-                                    <div class="ep">18 / ?</div>
-                                    <div class="view"><i class="fa fa-eye"></i> 9141</div>
-                                    <h5><a href="#">The Seven Deadly Sins: Wrath of the Gods</a></h5>
-                                </div>
-                                <div class="product__sidebar__view__item set-bg mix week years"
-                                     data-setbg="img/sidebar/tv-3.jpg">
-                                    <div class="ep">18 / ?</div>
-                                    <div class="view"><i class="fa fa-eye"></i> 9141</div>
-                                    <h5><a href="#">Sword art online alicization war of underworld</a></h5>
-                                </div>
-                                <div class="product__sidebar__view__item set-bg mix years month"
-                                     data-setbg="img/sidebar/tv-4.jpg">
-                                    <div class="ep">18 / ?</div>
-                                    <div class="view"><i class="fa fa-eye"></i> 9141</div>
-                                    <h5><a href="#">Fate/stay night: Heaven's Feel I. presage flower</a></h5>
-                                </div>
-                                <div class="product__sidebar__view__item set-bg mix day"
-                                     data-setbg="img/sidebar/tv-5.jpg">
-                                    <div class="ep">18 / ?</div>
-                                    <div class="view"><i class="fa fa-eye"></i> 9141</div>
-                                    <h5><a href="#">Fate stay night unlimited blade works</a></h5>
-                                </div>
-                            </div>
+                <div class="product__sidebar__comment">
+                    <div class="section-title">
+                        <h5>New Comment</h5>
+                    </div>
+                    <div class="product__sidebar__comment__item">
+                        <div class="product__sidebar__comment__item__pic">
+                            <img src="img/sidebar/comment-1.jpg" alt="">
                         </div>
-                        <div class="product__sidebar__comment">
-                            <div class="section-title">
-                                <h5>New Comment</h5>
-                            </div>
-                            <div class="product__sidebar__comment__item">
-                                <div class="product__sidebar__comment__item__pic">
-                                    <img src="img/sidebar/comment-1.jpg" alt="">
-                                </div>
-                                <div class="product__sidebar__comment__item__text">
-                                    <ul>
-                                        <li>Active</li>
-                                        <li>Movie</li>
-                                    </ul>
-                                    <h5><a href="#">The Seven Deadly Sins: Wrath of the Gods</a></h5>
-                                    <span><i class="fa fa-eye"></i> 19.141 Viewes</span>
-                                </div>
-                            </div>
-                            <div class="product__sidebar__comment__item">
-                                <div class="product__sidebar__comment__item__pic">
-                                    <img src="img/sidebar/comment-2.jpg" alt="">
-                                </div>
-                                <div class="product__sidebar__comment__item__text">
-                                    <ul>
-                                        <li>Active</li>
-                                        <li>Movie</li>
-                                    </ul>
-                                    <h5><a href="#">Shirogane Tamashii hen Kouhan sen</a></h5>
-                                    <span><i class="fa fa-eye"></i> 19.141 Viewes</span>
-                                </div>
-                            </div>
-                            <div class="product__sidebar__comment__item">
-                                <div class="product__sidebar__comment__item__pic">
-                                    <img src="img/sidebar/comment-3.jpg" alt="">
-                                </div>
-                                <div class="product__sidebar__comment__item__text">
-                                    <ul>
-                                        <li>Active</li>
-                                        <li>Movie</li>
-                                    </ul>
-                                    <h5><a href="#">Kizumonogatari III: Reiket su-hen</a></h5>
-                                    <span><i class="fa fa-eye"></i> 19.141 Viewes</span>
-                                </div>
-                            </div>
-                            <div class="product__sidebar__comment__item">
-                                <div class="product__sidebar__comment__item__pic">
-                                    <img src="img/sidebar/comment-4.jpg" alt="">
-                                </div>
-                                <div class="product__sidebar__comment__item__text">
-                                    <ul>
-                                        <li>Active</li>
-                                        <li>Movie</li>
-                                    </ul>
-                                    <h5><a href="#">Monogatari Series: Second Season</a></h5>
-                                    <span><i class="fa fa-eye"></i> 19.141 Viewes</span>
-                                </div>
-                            </div>
+                        <div class="product__sidebar__comment__item__text">
+                            <ul>
+                                <li>Active</li>
+                                <li>Movie</li>
+                            </ul>
+                            <h5><a href="#">The Seven Deadly Sins: Wrath of the Gods</a></h5>
+                            <span><i class="fa fa-eye"></i> 19.141 Viewes</span>
+                        </div>
+                    </div>
+                    <div class="product__sidebar__comment__item">
+                        <div class="product__sidebar__comment__item__pic">
+                            <img src="img/sidebar/comment-2.jpg" alt="">
+                        </div>
+                        <div class="product__sidebar__comment__item__text">
+                            <ul>
+                                <li>Active</li>
+                                <li>Movie</li>
+                            </ul>
+                            <h5><a href="#">Shirogane Tamashii hen Kouhan sen</a></h5>
+                            <span><i class="fa fa-eye"></i> 19.141 Viewes</span>
+                        </div>
+                    </div>
+                    <div class="product__sidebar__comment__item">
+                        <div class="product__sidebar__comment__item__pic">
+                            <img src="img/sidebar/comment-3.jpg" alt="">
+                        </div>
+                        <div class="product__sidebar__comment__item__text">
+                            <ul>
+                                <li>Active</li>
+                                <li>Movie</li>
+                            </ul>
+                            <h5><a href="#">Kizumonogatari III: Reiket su-hen</a></h5>
+                            <span><i class="fa fa-eye"></i> 19.141 Viewes</span>
+                        </div>
+                    </div>
+                    <div class="product__sidebar__comment__item">
+                        <div class="product__sidebar__comment__item__pic">
+                            <img src="img/sidebar/comment-4.jpg" alt="">
+                        </div>
+                        <div class="product__sidebar__comment__item__text">
+                            <ul>
+                                <li>Active</li>
+                                <li>Movie</li>
+                            </ul>
+                            <h5><a href="#">Monogatari Series: Second Season</a></h5>
+                            <span><i class="fa fa-eye"></i> 19.141 Viewes</span>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
+        </div>
         </div>
     </section>
 
